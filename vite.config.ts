a63d6e01cd7cb1b34f35d79bcc32e746091d70ed
Himanshu Sharma <imyselfhimanshusharma@gmail.com>
2026-01-20 12:28:30 +0530

    deploying

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: '/iamhimanshusharma.github.io',
+  base: '/iamhimanshusharma.github.io/',
   resolve: {
     alias: {
       // Alias @ to the src directory
